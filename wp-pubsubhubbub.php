3768ea2f1ff51d7146e6e6cd81120c7dcd433262
Pádraic Brady <padraic.brady@yahoo.com>
2009-08-06 01:51:28 +0100

    Few changes - will debug and finish tomorrow

--- wp-pubsubhubbub.php
+++ wp-pubsubhubbub.php
@@ -2,14 +2,41 @@
 /*
 Plugin Name: WP Pubsubhubbub
 Plugin URI: http://code.google.com/p/pubsubhubbub/
-Description: Implements a Pubsubhubbub Real-Time Publisher informing Planet Earth
-of your blog updates now, not later, with support for multiple Hubs and the most
-recent emerging practices (even if not in the Specification yet).
+Description: Implements a Pubsubhubbub Real-Time Publisher informing Planet Earth of your blog updates now, not later, with support for multiple Hubs and the most recent emerging practices (even if not in the Specification yet).
 Version: 1.0a
 Author: Padraic Brady
 Author URI: http://blog.astrumfutura.com
 */
 
+/**
+Copyright (c) 2009, Padraic Brady
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+
+    * Neither the name of Padraic Brady nor the names of its
+      contributors may be used to endorse or promote products derived from this
+      software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /**
  * Set up a functioning path for loading the Pubsubhubbub library files
@@ -41,13 +68,13 @@ function wpsh_notify_hubs($postId) {
         $feeds = array_unique(array(
             get_bloginfo('rss2_url'), get_bloginfo('atom_url')
         ));
-        $publisher->addTopicUrls($feeds);
+        $publisher->addUpdatedTopicUrls($feeds);
         $publisher->addHubUrls(wpsh_get_hubs());
         $publisher->notifyAll();
         if (!$publisher->isSuccess()) {
             $errors = $publisher->getErrors();
             foreach ($errors as $error) {
-
+                print_r($error['response']->getBody());
             }
         }
     } catch (Exception $e) {
@@ -88,7 +115,7 @@ function wpsh_add_atom_links($rss = false) {
     if ($rss) {
         $namespace = 'atom:';
     }
-    $hubs = get_hubs();
+    $hubs = wpsh_get_hubs();
     foreach ($hubs as $url) {
         echo '<', $namespace,'link rel="hub" href="', $url, '" />';
     }
