a94cd053b91f8cfc1543bb74c4d0c9edaf976c60
Pádraic Brady <padraic.brady@yahoo.com>
2009-08-06 16:37:16 +0100

    Stablised plugin, added settings page template, cut out unneeded Zend classes

--- wp-pubsubhubbub.php
+++ wp-pubsubhubbub.php
@@ -2,10 +2,11 @@
 /*
 Plugin Name: WP Pubsubhubbub
 Plugin URI: http://code.google.com/p/pubsubhubbub/
-Description: Implements a Pubsubhubbub Real-Time Publisher informing Planet Earth of your blog updates now, not later, with support for multiple Hubs and the most recent emerging practices (even if not in the Specification yet).
-Version: 1.0a
+Description: Implements a Pubsubhubbub Real-Time Publisher informing Planet Earth of your blog updates now, not later, with support for multiple Hubs and the most recent emerging practices. Edit the Hubs in use on the <a href="./options-general.php?page=wp-pubsubhubbub/wp-pubsubhubbub">WP Pubsubhubbub settings page</a>
+Version: 1.0
 Author: Padraic Brady
-Author URI: http://blog.astrumfutura.com
+Author Email: padraic.brady@yahoo.com
+Author URI: http://www.survivethedeepend.com
 */
 
 /**
@@ -42,17 +43,17 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * Set up a functioning path for loading the Pubsubhubbub library files
  * needed. Only those required are packaged, imported from latest svn/git HEAD.
  */
-$path = dirname(__FILE__);
-set_include_path(get_include_path() . PATH_SEPARATOR . $path . '/library');
+define('WPPSH_LIBRARY', dirname(__FILE__) . '/library');
+set_include_path(get_include_path() . PATH_SEPARATOR . WPPSH_LIBRARY);
 require_once 'Zend/Pubsubhubbub/Publisher.php';
 
-// All custom functions prefixed with "wpsh_" to avoid function name clashes
+// All custom functions/options prefixed with "wppsh_" to avoid name clashes
 
 /**
  * Add an action hook to initiate Pubsubhubbub Publisher notifications
  * to all configured Hubs
  */
-add_action('publish_post', 'wpsh_notify_hubs');
+add_action('publish_post', 'wppsh_notify_hubs');
 
 /**
  * Issue a notification to all utilised Hubs about the current update. Seems
@@ -62,14 +63,14 @@ add_action('publish_post', 'wpsh_notify_hubs');
  *
  * Subscribers should be encouraged to subscribe only the Atom 1.0 and RSS 2.0 feeds.
  */
-function wpsh_notify_hubs($postId) {
+function wppsh_notify_hubs($postId) {
     try {
         $publisher = new Zend_Pubsubhubbub_Publisher;
         $feeds = array_unique(array(
             get_bloginfo('rss2_url'), get_bloginfo('atom_url')
         ));
         $publisher->addUpdatedTopicUrls($feeds);
-        $publisher->addHubUrls(wpsh_get_hubs());
+        $publisher->addHubUrls(explode("\n", trim(wppsh_get_hubs())));
         $publisher->notifyAll();
         if (!$publisher->isSuccess()) {
             $errors = $publisher->getErrors();
@@ -89,12 +90,12 @@ function wpsh_notify_hubs($postId) {
  * This is a convenient default but the user should be encouraged to
  * deliberately select this, or other Hubs for clarity.
  */
-function wpsh_get_hubs() {
-    $hubs = get_option('hub_urls');
-    if (!$hubs) {
-        return array('http://pubsubhubbub.appspot.com');
+function wppsh_get_hubs() {
+    $hub = get_option('wppsh_hub_urls');
+    if (!$hub) {
+        return 'http://pubsubhubbub.appspot.com';
     } else {
-        return explode("\n", trim($hubs));
+        return $hub;
     }
 }
 
@@ -107,59 +108,34 @@ function wpsh_get_hubs() {
  *
  * Callback functions are declared after this list.
  */
-add_action('atom_head', 'wpsh_add_atom_links');
-add_action('rss_head', 'wpsh_add_rss_links');
+add_action('atom_head', 'wppsh_add_atom_links');
+add_action('rss2_head', 'wppsh_add_rss_links');
 
-function wpsh_add_atom_links($rss = false) {
+function wppsh_add_atom_links($rss = false) {
     $namespace = '';
     if ($rss) {
         $namespace = 'atom:';
     }
-    $hubs = wpsh_get_hubs();
+    $hubs = explode("\n", trim(wppsh_get_hubs()));
     foreach ($hubs as $url) {
         echo '<', $namespace,'link rel="hub" href="', $url, '" />';
     }
 }
 
-function wpsh_add_rss_links() {
-    wpsh_add_atom_links(true);
+function wppsh_add_rss_links() {
+    wppsh_add_atom_links(true);
 }
 
 /**
  * Create Administration Interface Hook and a function to write out
  * the necessary HTML.
  */
-add_action('admin_menu', 'wpsh_include_options_page');
+add_action('admin_menu', 'wppsh_include_options_page');
 
-function wpsh_include_options_page() {
-    add_options_page('WP Pubsubhubbub Settings', 'WP Pubsubhubbub', 8, __FILE__, 'wpsh_write_options_page');
+function wppsh_include_options_page() {
+    add_options_page('PubSubHubbub Settings', 'PubSubHubbub', 8, __FILE__, 'wppsh_write_options_page');
 }
 
-function wpsh_write_options_page() {
-    $hubs = implode("\n", wpsh_get_hubs());
-    $submitText = __('Save Changes');
-    $wpnonce = wp_nonce_field('update-options', null, null, false);
-    $page = <<<OPTIONS
-<div class="wrap">
-<h2>Configure one or more Hub Endpoint URLs to receive update pings</h2>
-<form method="post" action="options.php">
-$wpnonce
-<table class="form-table">
-<tr valign="top">
-<th scope="row">Hub Endpoint Urls</th>
-<td>
-<p>Add each Hub Endpoint Url to a new line:<br /></p>
-<textarea name="hub_urls" cols="40%" rows="8">$hubs</textarea>
-</td>
-</tr>
-</table>
-<input type="hidden" name="action" value="update" />
-<input type="hidden" name="page_options" value="hub_urls" />
-<p class="submit">
-<input type="submit" class="button-primary" value="$submitText" />
-</p>
-</form>
-</div>
-OPTIONS;
-    echo $page;
+function wppsh_write_options_page() {
+    include 'options.phtml';
 }
